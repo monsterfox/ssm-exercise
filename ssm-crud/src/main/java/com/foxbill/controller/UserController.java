6f33f765087a2610540d1d8ec5239d1b3b015b9e
foxbill <565197135@qq.com>
2022-07-31 09:17:27 +0800

    完成ssm-crud的json转换

--- ssm-crud/src/main/java/com/foxbill/controller/UserController.java
+++ ssm-crud/src/main/java/com/foxbill/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,6 +27,12 @@ public class UserController {
         mv.addObject("list",userService.findAll());
         mv.setViewName("list");
         return mv;
+    }
+
+    @RequestMapping("/list")
+    @ResponseBody
+    public List<User> list(){
+        return userService.findAll();
     }
 
     @RequestMapping("/toAddUser")
